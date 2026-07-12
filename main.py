1ec82fc1551f9ce0b53a93eb8d43432abbc2d3dd
Kaustubh Srivastav <kaustubhs1517@gmail.com>
2026-07-12 20:25:57 +0530

    Added delete student feature

--- main.py
+++ main.py
@@ -35,13 +35,42 @@ def view_students():
 
     print()
 
+def search_student():
+    print("\n----- Search Student -----")
+    roll = input("Enter roll no. to search: ")
+    for student in students:
+        if student["roll"] == roll:
+            print(f"\nStudent found!")
+            print("------------------------")
+            print(f"Name   : {student['name']}")
+            print(f"Roll   : {student['roll']}")
+            print(f"Course : {student['course']}")
+            print()
+            return
+    print("\n❌ Student not found.\n")
 
+
+def delete_student():
+
+    print("\n----- Delete Student -----")
+
+    roll = input("Enter roll number to delete: ")
+
+    for student in students:
+        if student["roll"] == roll:
+            students.remove(student)
+            print("\n✅ Student deleted successfully!\n")
+            return
+
+    print("\n❌ Student not found.\n")
 while True:
 
     print("========== Student Management System ==========")
     print("1. Add Student")
     print("2. View Students")
-    print("3. Exit")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
@@ -52,8 +81,13 @@ while True:
         view_students()
 
     elif choice == "3":
+        search_student()
+
+    elif choice == "4":
+        delete_student()
+
+    elif choice == "5":
         print("Thank you for using the system!")
         break
-
     else:
         print("Invalid choice.\n")
